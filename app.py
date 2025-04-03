16b73ba150ab35616ad2847bc832ed606013c053
ribunnan <165739167+ribunnan@users.noreply.github.com>
2025-04-03 17:34:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,6 @@ def get_word_info_api():
     return jsonify(info)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
